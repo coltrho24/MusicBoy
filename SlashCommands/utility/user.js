7c4a7510fb06cdf46ba11501a91953d857a73011
Colton Rhodes <colton.rhodes@oit.edu>
2024-05-19 21:36:50 -0700

    base bot is up and running, now ready to make it a music bot

---
+++ SlashCommands/utility/user.js
@@ -0,0 +1,11 @@
+const {SlashCommandBuilder} = require('discord.js');
+const { execute } = require('./ping');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('Provides the users information'),
+    async execute(interaction){
+        await interaction.reply(`This command was ran by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}`);
+    },
+};
